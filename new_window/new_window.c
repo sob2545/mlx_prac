a8889425ca3d91dc53cf11e26e2a38933dd568a4
Seongmin Sim <sesim@c1r7s1.42seoul.kr>
2022-06-25 08:12:28 +0900

    new_window

---
+++ new_window/new_window.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   new_window.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sesim <sesim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 11:20:07 by sesim             #+#    #+#             */
+/*   Updated: 2022/06/25 08:11:48 by sesim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../mlx/mlx.h"
+#include <stdlib.h>
+#include <math.h>
+
+int	main(void)
+{
+	void	*mlx;
+	void	*win;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 500, 500, "test");
+	mlx_loop(mlx);
+}
